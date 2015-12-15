97a442613bb5a48db9adc699d409d89afe8070fb
Tulino, Ryan <rtulino@ebay.com>
2015-12-15 14:47:54 -0500

    use sinatra

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Rotary Commerce, Framework
+# Rotary
 
 This repository contains the framework for Rotary, an Ecommerce Platform hastily built in Ruby
+
+## Install
+
+This project requires sinatra:
+
+`gem install sinatra`
+
+## Configure
+
+`/..`
+
+## Run
+
+Daemonize the API server:
+
+`ruby rotary.rb`
+
+## Usage
+
+Make a basic request:
+
+`curl localhost:4567`
--- src/rotary.rb
+++ src/rotary.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
 require_relative 'app/bootstrap'
 
-require_relative 'app/models/input_validator'
-validator = InputValidator.new
+# require_relative 'app/models/input_validator'
+# validator = InputValidator.new
+#
+# require_relative 'app/controllers/question_controller'
+# QuestionController.new(
+#     :arguments    => Proc.new { ARGV },
+#     :input_stream => Proc.new { STDIN },
+#     :validator    => Proc.new { validator }
+# ).ask
+require 'sinatra'
 
-require_relative 'app/controllers/question_controller'
-QuestionController.new(
-    :arguments    => Proc.new { ARGV },
-    :input_stream => Proc.new { STDIN },
-    :validator    => Proc.new { validator }
-).ask
+get '/' do
+  'Hello Rotary!'
+end
+
+
